1f2db5fc2695e82124d4d2ccd64e5fdfa5082275
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2021-03-31 16:24:21 +0530

    Created controller

---
+++ src/main/java/com/abhishekashyap/backend_st2_upgrad/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.abhishekashyap.backend_st2_upgrad.controller;
+
+import com.abhishekashyap.backend_st2_upgrad.model.User;
+import com.abhishekashyap.backend_st2_upgrad.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/addUser")
+    public User storeUser(@RequestBody User aUser) {
+        return userRepository.save(aUser);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
