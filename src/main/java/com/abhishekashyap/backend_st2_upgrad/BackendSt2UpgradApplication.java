b005a7de8cfcbd320e6a7b5b82eca2500bb42852
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2021-03-31 16:20:42 +0530

    Initializr files

---
+++ src/main/java/com/abhishekashyap/backend_st2_upgrad/BackendSt2UpgradApplication.java
@@ -0,0 +1,13 @@
+package com.abhishekashyap.backend_st2_upgrad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSt2UpgradApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSt2UpgradApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abhishekashyap/backend_st2_upgrad/BackendSt2UpgradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishekashyap.backend_st2_upgrad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSt2UpgradApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
